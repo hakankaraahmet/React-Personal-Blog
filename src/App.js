cb654fbdce36ac9d6a52d8656444b1786a6fdca6
hakankaraahmet <karaahmethkn@gmail.com>
2021-09-21 22:57:05 +0300

    modified app js with çalışmalarım.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hakkımda from './components/Hakkımda'
 import Navbar from "./components/Navbar"
+import Çalışmalarım from './components/Çalışmalarım'
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hakkımda/>
+      <Çalışmalarım/>
     </div>
   )
 }
