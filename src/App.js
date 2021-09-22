5261c9cac2a6d910ac821686c5093ad7b9321603
hakankaraahmet <karaahmethkn@gmail.com>
2021-09-22 09:22:41 +0300

    app.js modified with yorumlar.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Hakkımda from './components/Hakkımda'
 import Navbar from "./components/Navbar"
 import Çalışmalarım from './components/Çalışmalarım'
+import Yorumlar from "./components/Yorumlar"
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hakkımda/>
       <Çalışmalarım/>
+      <Yorumlar/>
     </div>
   )
 }
