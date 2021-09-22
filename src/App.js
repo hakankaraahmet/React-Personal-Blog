a690f2e9ae002399b828cd125372856d9a697e5a
hakankaraahmet <karaahmethkn@gmail.com>
2021-09-22 12:43:13 +0300

    modified App.js with functions and states

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import Hakkımda from './components/Hakkımda'
-import Navbar from "./components/Navbar"
-import Çalışmalarım from './components/Çalışmalarım'
-import Yorumlar from "./components/Yorumlar"
+import React, { useState } from "react";
+import Hakkımda from "./components/Hakkımda";
+import Navbar from "./components/Navbar";
+import Çalışmalarım from "./components/Çalışmalarım";
+import Yorumlar from "./components/Yorumlar";
+import InitialState from "./components/InitialState";
 const App = () => {
+  const [yorumlar, setYorumlar] = useState(InitialState);
+
+  
+  const yorumEkle = (yorum) => {
+    const id = Date.now();
+    const yeniYorum = { ...yorum, id };
+    setYorumlar((prevState) => [...prevState, yeniYorum]);
+  };
+
+ 
+
+  const yorumSil = (id) => {
+    setYorumlar(yorumlar.filter((yorum) => id !== yorum.id));
+  };
+
   return (
     <div>
-      <Navbar/>
-      <Hakkımda/>
-      <Çalışmalarım/>
-      <Yorumlar/>
+      <Navbar />
+      <Hakkımda />
+      <Çalışmalarım />
+      <Yorumlar yorumlar={yorumlar} yorumSil={yorumSil} yorumEkle={yorumEkle}/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
