8ed2b8102df693ce20a7d9af3f0afd98177404f9
hakankaraahmet <karaahmethkn@gmail.com>
2021-09-22 12:43:55 +0300

    created Initial state

---
+++ src/components/InitialState.js
@@ -0,0 +1,17 @@
+ const InitialState = [
+    {
+      id: 1,
+      mesaj: "güzel bir çalışma",
+      isim:"hakan",
+      email:"karaahmethkn@gmail.com"
+    },
+    {
+      id: 2,
+      mesaj: "oldukça beğendim",
+      isim:"lütfullahclnk",
+      email:"lutfullahclnk1104@gmail.com"
+      
+    },
+];
+
+export default InitialState
